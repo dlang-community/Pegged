75a784e110e611a55e284f4244079ff2d2559005
Philippe Sigaud <philippe.sigaud@gmail.com>
2012-03-11 00:17:36 +0100

    Testing the wiki examples.

--- test.d
+++ test.d
@@ -9,14 +9,45 @@ import std.typetuple;
 
 import pegged.grammar;
 
-import pegged.examples.constraints;
 
-mixin(grammar(
-    "A <- [0-9]"
-      ));
+import std.array;
+string[] nameStack;
 
+Output opening(Output o)
+{
+    nameStack ~= o.capture[0];
+    return o;
+}
+
+Output closing(Output o)
+{
+    if (nameStack.back != o.capture[0])
+        o.success = false;
+    else
+        nameStack.popBack;
+    return o;
+}
+
+
+
+mixin(grammar(`    
+    Node       <- OpeningTag{opening} (Node / Text)* ClosingTag{closing}
+    OpeningTag <- :"<">Identifier>:">" 
+    ClosingTag <- :"</">Identifier>:">"
+    Text       <~ (!OpeningTag>!ClosingTag>.)*
+`));
+
+mixin(grammar(`
+
+List(Elem, Sep) <- Elem (Sep Elem)*
+List(Elem)      <- List(Elem, :',')
+`));
 
 void main()
 {
-    writeln(A.parse("0a"));
+    auto p = Node.parse("<a> Hello <b> World </b> ! </a>");
+    writeln(p);
+    writeln(nameStack);
+    
+    writeln(List!Identifier.parse("Hello,World,!"));
 }
