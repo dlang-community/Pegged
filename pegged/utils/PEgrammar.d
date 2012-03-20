bb5301ea75e6484bec87edb7a04edf7fb4cc9a30
Philippe Sigaud <philippe.sigaud@gmail.com>
2012-03-20 21:36:58 +0100

    Updating the grammar to deal with escape sequences in char ranges.

--- pegged/utils/PEgrammar.d
+++ pegged/utils/PEgrammar.d
@@ -1,58 +1,54 @@
 module pegged.utils.PEgrammar;
 
-
 enum string PEG =`
-Grammar    <- S Definition+ EOI
-Definition <- RuleName Arrow Expression S
-RuleName   <- Identifier (ParamList?) S
-Expression <- Sequence (OR Sequence)*
-Sequence   <- Prefix+
-Prefix     <- (LOOKAHEAD / NOT / DROP / FUSE)? Suffix
-Suffix     <- Primary ( OPTION 
-                      / ONEORMORE 
-                      / ZEROORMORE 
-                      / NamedExpr 
-                      / WithAction)? S
-Primary    <- Name !Arrow
-            / GroupExpr
-            / Literal 
-            / Class 
-            / ANY
-Name       <- QualifiedIdentifier (ArgList?) S
-GroupExpr  <- :OPEN Expression :CLOSE S
-
-Literal    <~ :Quote (!Quote Char)* :Quote S
-            / :DoubleQuote (!DoubleQuote Char)* :DoubleQuote S
-Class      <- :'[' (!']' CharRange)* :']' S
-CharRange  <- Char :'-' Char / Char
-Char       <~ BackSlash [nrt]
-            / !BackSlash .
-
-ParamList  <~ OPEN Identifier (',' Identifier)* CLOSE S
-ArgList    <- :OPEN Expression (:',' Expression)* :CLOSE S
-NamedExpr  <- NAME Identifier? S
-
-WithAction <~ :ACTIONOPEN Identifier :ACTIONCLOSE S
+Grammar     <- S Definition+ EOI
+Definition  <- RuleName Arrow Expression S
+RuleName    <- Identifier (ParamList?) S
+Expression  <- Sequence (OR Sequence)*
+Sequence    <- Prefix+
+Prefix      <- (LOOKAHEAD / NOT / DROP / FUSE)? Suffix
+Suffix      <- Primary ( OPTION 
+                       / ONEORMORE 
+                       / ZEROORMORE 
+                       / NamedExpr 
+                       / WithAction)? S
+Primary     <- Name !Arrow
+             / GroupExpr
+             / Literal 
+             / Class 
+             / ANY
+Name        <- QualifiedIdentifier ArgList? S
+GroupExpr   <- :OPEN Expression :CLOSE S
+Literal     <~ :Quote (!Quote Char)* :Quote S
+             / :DoubleQuote (!DoubleQuote Char)* :DoubleQuote S
+Class       <- :'[' (!']' CharRange)* :']' S
+CharRange   <- Char :'-' Char / Char
+Char        <~ BackSlash ('-' / BackSlash / '[' / ']')
+             / !BackSlash .
+ParamList   <~ OPEN Identifier (',' Identifier)* CLOSE S
+ArgList     <- :OPEN Expression (:',' Expression)* :CLOSE S
+NamedExpr   <- NAME Identifier? S
+WithAction  <~ :ACTIONOPEN Identifier :ACTIONCLOSE S
     
-Arrow      <- LEFTARROW / FUSEARROW / DROPARROW / ACTIONARROW / SPACEARROW
-LEFTARROW  <- "<-" S
-FUSEARROW  <- "<~" S
-DROPARROW  <- "<:" S
-ACTIONARROW <- "<">WithAction S
-SPACEARROW <- "<" S
+Arrow       <- LEFTARROW / FUSEARROW / DROPARROW / ACTIONARROW / SPACEARROW
+LEFTARROW   <- "<-" S
+FUSEARROW   <- "<~" S
+DROPARROW   <- "<:" S
+ACTIONARROW <- "<" WithAction S
+SPACEARROW  <- "<" S
   
-OR         <- '/' S
+OR          <- '/' S
     
-LOOKAHEAD  <- '&' S
-NOT        <- '!' S
+LOOKAHEAD   <- '&' S
+NOT         <- '!' S
 
-DROP       <- ':' S
-FUSE       <- '~' S
+DROP        <- ':' S
+FUSE        <- '~' S
   
 #SPACEMUNCH <- '>' S
     
-NAME       <- '=' S
-ACTIONOPEN <- '{' S
+NAME        <- '=' S
+ACTIONOPEN  <- '{' S
 ACTIONCLOSE <- '}' S
     
 OPTION     <- '?' S
@@ -64,7 +60,7 @@ CLOSE      <- ')' S
     
 ANY        <- '.' S
     
-S          <: (Blank / EOL / Comment)*
-Comment    <- "#" (!EOL>.)* (EOL/EOI)
+S          <: (Spacing / Comment)*
+Comment    <- "#" (!EOL .)* (EOL/EOI)
 `;
 
