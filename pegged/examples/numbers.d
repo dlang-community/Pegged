d60eafa2f80ea51253e59da38ebe093f2a49da97
Philippe Sigaud <philippe.sigaud@gmail.com>
2012-03-24 16:37:36 +0100

    Adding unittests to pegged/examples/numbers.d

--- pegged/examples/numbers.d
+++ pegged/examples/numbers.d
@@ -3,12 +3,32 @@ module pegged.examples.numbers;
 import pegged.grammar;
 
 /// Numbers
-mixin(grammar(
-   `Number   <- Floating ( ('e' / 'E' ) Integer )?
-    Floating <- Integer ('.' Unsigned )?
-    Unsigned <- [0-9]+
-    Integer  <- Sign Unsigned
-    Hexa     <- [0-9a-f]+ / [0-9A-F]+ 
-    Sign     <- ('-' / '+')?`
+mixin(grammar(`
+Numbers:
+    Scientific <- Floating ( ('e' / 'E' ) Integer )?
+    Floating   <- Integer ('.' Unsigned )?
+    Unsigned   <- [0-9]+
+    Integer    <- Sign Unsigned
+    Hexa       <- [0-9a-f]+ / [0-9A-F]+ 
+    Sign       <- ('-' / '+')?`
 ));
 
+unittest
+{
+    assert(Numbers.parse("123").success);
+    assert(Numbers.parse("123.0").success);
+    assert(Numbers.parse("123.01").success);
+    assert(Numbers.parse("123.").success);
+    assert(Numbers.parse("-123").success);
+    assert(Numbers.parse("-123.0").success);
+    assert(Numbers.parse("+123").success);
+    assert(Numbers.parse("-123e+12").success);
+    assert(Numbers.parse("+123E-12").success);
+    assert(Numbers.parse("123.456e+00").success);
+    assert(Numbers.Hexa.parse("DEADBEEF").success);
+    assert(Numbers.Hexa.parse("0123BEEF").success);
+    assert(Numbers.Hexa.parse("DEAD0123").success);
+    
+    assert(!Numbers.parse("").success);
+    assert(!Numbers.parse("abc").success);
+}
