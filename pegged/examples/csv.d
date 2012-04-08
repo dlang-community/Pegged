107465f4b81f9e4a5eab3896ec94fe2d8bcf4642
Philippe Sigaud <philippe.sigaud@gmail.com>
2012-04-08 20:52:31 +0200

    CSV: adding unit tests.

--- pegged/examples/csv.d
+++ pegged/examples/csv.d
@@ -1,6 +1,8 @@
 module pegged.examples.csv;
 
-enum CSVGrammar = "
+import pegged.grammar;
+
+mixin(grammar(`
 CSV:
 
 CsvFile         <- Line* LastLine
@@ -15,7 +17,33 @@ QuotedField     <- DoubleQuote EscapedField DoubleQuote
 EscapedField    <- SubField (DoubleQuote DoubleQuote SubField)*
 SubField        <- (!DoubleQuote !EOI .)+
 
-S               <: ' ' / '\t'
-";
+S               <: ' ' / '  '
+`));
 
-
+unittest
+{
+    assert(CSV.parse("1,2,3").success);
+    assert(CSV.parse("1,2,3,").success);
+    assert(CSV.parse("1,2,,3").success);
+    assert(CSV.parse("1,,,,,").success);
+    assert(CSV.parse(",1").success);
+    assert(CSV.parse("").success);
+    auto p1 = CSV.parse(
+"1, 2 , 3
+ 4, 5,  6,,
+ 7");
+    
+    
+    assert(p1.children[0].children.length == 3); // 3 lines
+    assert(p1.children[0].children[0].capture == ["1","2","3"]);
+    assert(p1.children[0].children[1].capture == ["4","5","6","",""]);
+    assert(p1.children[0].children[2].capture == ["7"]); 
+    
+    assert(CSV.parse("1,
+                     2,
+                     3,
+                     4,
+                     ").success);
+    
+    assert(!CSV.parse("1 2 3").success);
+}
