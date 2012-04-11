269ada3ba26d20468d3064b152a4039b68d31810
Philippe Sigaud <philippe.sigaud@gmail.com>
2012-04-11 23:19:02 +0200

    Bug correction int the Numbers grammar.

--- pegged/examples/numbers.d
+++ pegged/examples/numbers.d
@@ -5,13 +5,13 @@ import pegged.grammar;
 /// Numbers
 mixin(grammar(`
 Numbers:
-    Scientific <- Floating ( ('e' / 'E' ) Integer )?
-    Floating   <- Integer ('.' Unsigned )?
-    Unsigned   <- [0-9]+
-    Integer    <- Sign Unsigned
-    Hexa       <- [0-9a-f]+ / [0-9A-F]+ 
-    Sign       <- ('-' / '+')?`
-));
+    Scientific <~ Floating ( ('e' / 'E' ) Integer )?
+    Floating   <~ Integer ('.' Unsigned )?
+    Unsigned   <~ [0-9]+
+    Integer    <~ Sign? Unsigned
+    Hexa       <~ [0-9a-fA-F]+ 
+    Sign       <- '-' / '+'
+`));
 
 unittest
 {
