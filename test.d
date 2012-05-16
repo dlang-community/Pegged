98af6567f372c87592a3ad13d851851a07f83f8e
Philippe Sigaud <philippe.sigaud@gmail.com>
2012-05-16 21:49:22 +0200

    Testing eanderton problem.

--- test.d
+++ test.d
@@ -3,27 +3,103 @@ module test;
 import std.algorithm;
 import std.array;
 import std.conv;
+import std.range;
 import std.stdio;
-//import pegged.peg;
+import std.typecons;
+
 import pegged.grammar;
-//import pegged.examples.PEGGED;
 
-enum G = `
-Test:
-    A <- :DoubleQuote ~Text :DoubleQuote
-    Text <- (!DoubleQuote .)*
-`;
+version(none)
+{
+mixin(grammar(`
+RangeComprehension < Yield '|' Element (';' Element)*
+Yield    < ~(!'|' .)+
+Element  < Generate / Filter
+Generate < Identifier :"in" ~(!';' .)+
+Filter   <  ~(!';' .)+
+`));
+
+string comp(string input)
+{
+    auto tree = RangeComprehension.parse(input);
+    if (!tree.success) 
+        return "static assert(0, `Bad range comprehension input: `" ~ input ~ "`);";
+    string result;
+    return result;
+}
 
 
-mixin(grammar(G));
+class RC
+{
+    typeof(iota(long.max)) xRange;
+    ElementType!(typeof(xRange)) x;
+    typeof(iota(x)) yRange;
+    ElementType!(typeof(yRange)) y;
+    
+    this()
+    {
+        xRange = iota(long.max);
+        x = xRange.front;
+        yRange = iota(x);
+        y = yRange.front;
+    }
+    
+    bool empty() @property
+    {
+        return xRange.empty || yRange.empty;
+    }
+    
+    void popFront() @property
+    {
+        while(!yRange.empty && !(y*y<x))
+        {
+            yRange.popFront();
+            y = yRange.front;
+        }
+
+        if (yRange.empty)
+        {
+            xRange.popFront();
+            x = xRange.front;
+            yRange = iota(x);
+            if (!yRange.empty)
+                y = yRange.front;
+            while(!yRange.empty && !(y*y<x))
+            {
+                yRange.popFront();
+                y = yRange.front;
+            }
+        }
+        
+        auto y2 = filter!(y => y*y<x)(yRange);
+        writeln(y2);
+        
+    }
+    
+    typeof(tuple(x,y)) front() @property
+    {
+        return tuple(x,y);
+    }
+}
+
+class ItemGetter
+{
+    int opCall(out ItemGetter next)
+    {
+        return 0;
+    }
+}
+}
+
+
+import pegged.grammar;
+mixin(grammar(`Number < [0123456789]*`));
+enum result = Number.parse("123");
+
 
 void main()
 {
-    writeln(Test.validate(`"Hello, World!"`));
-    writeln(Test.match(`"Hello, World!"`));
-    writeln(Test.parse(`"Hello, World!"`));
-    writeln(Test.fullParse(`"Hello, World!"`));
-    writeln(Test.fullestParse(`"Hello, World!"`));
+    writeln(result);
 }
 
 
