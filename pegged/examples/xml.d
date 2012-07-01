e52a2af7e41e57b49525641bfb243195465c02a7
Philippe Sigaud <philippe.sigaud@gmail.com>
2012-07-01 16:08:29 +0200

    Changing the actions to template functions, to work with any kind of parse tree.

--- pegged/examples/xml.d
+++ pegged/examples/xml.d
@@ -6,10 +6,12 @@ module pegged.examples.xml;
 import pegged.grammar;
 
 import std.array;
-string[] nameStack;
+dstring[] nameStack;
+
+//alias pegged.peg.Output!(ParseTree) Output;
 
 /// Semantic action to push a tag name on a stack
-Output opening(Output o)
+O opening(O)(O o)
 {
     nameStack ~= o.capture[0];
     return o;
@@ -19,12 +21,12 @@ Output opening(Output o)
  * Semantic action to pop the name stack and compare a tag name
  * The parsing fails if the tags do not nest correctly (See docs, the Semantic Actions wiki page)
  */ 
-Output closing(Output o)
+O closing(O)(O o)
 {
     if (nameStack.back != o.capture[0])
         o.success = false;
     else
-        nameStack.popBack;
+        nameStack.popBack();
     return o;
 }
 
@@ -42,11 +44,12 @@ unittest
 
     auto p1 = XML.parse("<a></a>");
     assert(p1.success);
-    assert(p1.capture == ["a", "a"]);
-    assert(p1.children.length == 1);
-    assert(p1.children[0].name == "Node");
-    assert(p1.children[0].children[0].name == "OpeningTag");
-    assert(p1.children[0].children[1].name == "ClosingTag");
+    assert(p1.capture == ["a"d, "a"d]);
+    assert(p1.children.length == 2);
+    
+    assert(p1.name == "XML.Node"d);
+    assert(p1.children[0].name == "XML.OpeningTag"d);
+    assert(p1.children[1].name == "XML.ClosingTag"d);
 
     assert(!XML.parse("<a></>").success); // incomplete closing tag
     assert(!XML.parse("<a></b>").success); // unmatched tag
@@ -69,8 +72,8 @@ XML: [[index: 0, line: 0, col: 0] - [index: 73, line: 0, col: 73]]["text", "Hell
       Text: [[index: 52, line: 0, col: 52] - [index: 66, line: 0, col: 66]][" announcement!"]
       ClosingTag: [[index: 66, line: 0, col: 66] - [index: 73, line: 0, col: 73]]["text"]
 */      
-    assert(p2.capture == ["text", "Hello World! This is an ", "emph", "important", "emph", " announcement!", "text"]);
-    assert(p2.children[0].children[0].capture == ["text"]);
-    assert(p2.children[0].children[1].capture == ["Hello World! This is an "]);
-    assert(p2.children[0].children[$-1].capture == ["text"]);
+    assert(p2.capture == ["text"d, "Hello World! This is an "d, "emph"d, "important"d, "emph"d, " announcement!"d, "text"d]);
+    assert(p2.children[0].capture == ["text"d]);
+    assert(p2.children[1].capture == ["Hello World! This is an "d]);
+    assert(p2.children[$-1].capture == ["text"d]);
 }
