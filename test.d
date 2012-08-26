113053159e301360a99c003a614919a81593b895
Philippe Sigaud <philippe.sigaud@gmail.com>
2012-08-26 23:07:38 +0200

    Testing the Pegged parser (grammar file).

--- test.d
+++ test.d
@@ -9,27 +9,23 @@ import std.range;
 import std.stdio;
 import std.typecons;
 
-//import pegged.examples.xml2;
 import pegged.grammar;
-import dparser;
+//import pegged.examples.peggedgrammar;
+
+//import PeggedParser;
+//import _grammar;
 
 void main()
 {
-    writeln(D("
-module test.foo;
+    //asModule("_grammar", PEGGEDgrammar);
 
-int i,j;
-
-class C { this() {} }
-
-struct S(T) 
-{
-    T t;
-}
-
-void main()
-{
-    double[] array;
-}
+    mixin(grammar("
+    Test:
+        A <- B C*
+        B <- 'b'
+        C <- 'c'
     "));
+        
+    writeln(Test("bcccd"));
+    
 }
