82f4c33b1059a95f7b82c535661893cf80287f45
Philippe Sigaud <philippe.sigaud@gmail.com>
2012-09-15 12:04:06 +0200

    Testing module production while reading from a file.

--- test.d
+++ test.d
@@ -5,6 +5,8 @@ import std.algorithm;
 import std.array;
 import std.conv;
 import std.datetime;
+//import std.file;
+//import std.path;
 import std.range;
 import std.stdio;
 import std.typecons;
@@ -14,35 +16,11 @@ import pegged.grammar;
 import pegged.parser;
 import pegged.introspection;
 
+import arithmetic;
+    
+
 void main()
 {
-	enum g = "
-	Gram:
-		A <- 'a'? B*
-		B <- 'c'? A 'c'*
-		";
-	
-	mixin(grammar(g));
-	//asModule("G", g);
-	//mixin(grammar(g));
-	//writeln(Gram("abccc"));
-	//writeln(Gram.info);
-	//writeln(Pegged(G));
-	//writeln(callGraph(G));
-	//writeln(ruleInfo(G));
-	//writeln(ruleInfo(G));
-	
-	//writeln(grammar(G));
-	//asModule("G2", g);
-	//alias Gram.A!(literal!"b") Rule;
-	
-	//writeln(Rule("abbbc"));
-	//writeln(Gram.A!(literal!"b")("abbbc"));
-	
-	//writeln(S.A!("abc","def")("abc"));
-	
-	//writeln(Gram.ruleNames);
-	//writeln(Gram.info["A"].recursion);
-	//writeln(Gram.info["A"].nullMatch);
-	//writeln(Gram.info["A"].infiniteLoop);
+    //asModule("arithmetic", File("grammar.txt"));
+    writeln(Arithmetic("1+1"));
 }
