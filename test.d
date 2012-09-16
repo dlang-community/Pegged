7af2ccd285ba762bf3eb49fadbfc939aff8953d6
Philippe Sigaud <philippe.sigaud@gmail.com>
2012-09-16 14:44:06 +0200

    Testing the documenation code.

--- test.d
+++ test.d
@@ -21,10 +21,28 @@ import pegged.examples.pattern;
 
 void main()
 {
-    alias TypeTuple!(byte,int,double,int) I;
-    alias OneOrMore!(SubType!(ulong)) P;
-    alias P.Match!(I) R;
-    writeln(R.successful,": ", R.Types.stringof, " / ", R.Rest.stringof, "\n");
+    //alias TypeTuple!(byte,int,double,int) I;
+    //alias OneOrMore!(SubType!(ulong)) P;
+    //alias P.Match!(I) R;
+    //writeln(R.successful,": ", R.Types.stringof, " / ", R.Rest.stringof, "\n");
     
-    writeln(Pattern(`. {. first}`));
+    //writeln(Pattern(`. {. first}`));
+    alias zeroOrMore!(or!(literal!"abc", literal!"d")) rule; // in PEG-speak:  '("abc" / "d")*'
+ParseTree input = ParseTree("",false,[], "abcdabce");
+
+ParseTree result = rule(input);
+
+assert(result.successful);
+assert(result.matches == ["abc", "d", "abc"]);
+assert(result.end == 7); // matched "abcdabce"[0..7] => "abcdabc". "e" at the end is not part of the parse tree.
+assert(result.children.length == 3);
+writeln(result);
+/+
+writes:
++/
+
+input = ParseTree("",false,[], "efgh");
+result = rule(input);
+assert(result.successful); // succeed, even though all patterns failed.
+assert(result.children.length == 0);
 }
