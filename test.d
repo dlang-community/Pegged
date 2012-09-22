84a629966e61c2f155cdf3aa20f55f6b08771c18
Philippe Sigaud <philippe.sigaud@gmail.com>
2012-09-22 13:52:36 +0200

    Testing pattern-matching.

--- test.d
+++ test.d
@@ -18,31 +18,56 @@ import pegged.introspection;
 
 import pegged.examples.pattern;
 
+struct MatchFailure {};
+
+auto pair(Args...)(Args args) 
+{
+	alias isRange		 	P0;
+	alias DiscardMatch!Any	P1;
+	alias ZeroOrMore!(Any)	P2;
+	alias And!(P0,P1,P2) 	P;
+	
+	alias P.Match!(Args) M;
+	
+	static if(M.successful)
+	{
+		alias P0.Match!(Args) 		M0;
+		alias P1.Match!(M0.Rest) 	M1; // discarded
+		alias P2.Match!(M1.Rest) 	M2;
+		
+		struct MatchResult
+		{
+			M0.Types a;
+			M2.Types b;
+		}
+		return MatchResult(args[M0.begin .. M0.end], 
+		                   args[M0.end+M1.end+M2.begin .. M0.end+M1.end+M2.end]);
+	}
+	else
+		return MatchFailure();
+}
+
+
+
+class C
+{
+	int[] i;
+	double d;
+	string s;
+	char c;
+	
+	this(int[] i, double d, string s, char c)
+	{
+		this.i = i;
+		this.d = d;
+		this.s = s;
+		this.c = c;
+	}
+}
+
 
 void main()
 {
-    //alias TypeTuple!(byte,int,double,int) I;
-    //alias OneOrMore!(SubType!(ulong)) P;
-    //alias P.Match!(I) R;
-    //writeln(R.successful,": ", R.Types.stringof, " / ", R.Rest.stringof, "\n");
-    
-    //writeln(Pattern(`. {. first}`));
-    alias zeroOrMore!(or!(literal!"abc", literal!"d")) rule; // in PEG-speak:  '("abc" / "d")*'
-ParseTree input = ParseTree("",false,[], "abcdabce");
-
-ParseTree result = rule(input);
-
-assert(result.successful);
-assert(result.matches == ["abc", "d", "abc"]);
-assert(result.end == 7); // matched "abcdabce"[0..7] => "abcdabc". "e" at the end is not part of the parse tree.
-assert(result.children.length == 3);
-writeln(result);
-/+
-writes:
-+/
-
-input = ParseTree("",false,[], "efgh");
-result = rule(input);
-assert(result.successful); // succeed, even though all patterns failed.
-assert(result.children.length == 0);
+    	
+	writeln(literalValue!(0,1)(0,1,2));
 }
