731989379157b54b743a56c9e5fefde6c9e0d80b
Philippe Sigaud <philippe.sigaud@gmail.com>
2012-09-23 23:13:32 +0200

    Parameterized rules code needs to be modified again.

--- test.d
+++ test.d
@@ -16,7 +16,75 @@ import pegged.grammar;
 import pegged.parser;
 import pegged.introspection;
 
+struct GenericTest(TParseTree)
+{
+    struct Test
+    {
+    enum name = "Test";
+    static bool isRule(string s)
+    {
+        switch(s)
+        {
+            case "Test.A":
+            case "Test.B":
+                return true;
+            default:
+                return false;
+        }
+    }
+    mixin decimateTree;
+    alias spacing Spacing;
 
+    static TParseTree A(TParseTree p)
+    {
+        return named!(literal!("a"), name ~ ".A")(p);
+    }
+
+    static TParseTree A(string s)
+    {
+        return named!(literal!("a"), name ~ ".A")(TParseTree("", false,[], s));
+    }
+
+    template B(alias b)
+    {
+        static TParseTree B(T)(T t) if (is(T == TParseTree) || is(T == string))
+        {
+            static if (is(T == TParseTree))
+                return named!(b, name ~ ".B")(t);
+            else
+                return named!(b, name ~ ".B")(TParseTree("", false,[], t));
+        }
+
+        
+    }
+    
+    static TParseTree opCall(TParseTree p)
+    {
+        TParseTree result = decimateTree(A(p));
+        result.children = [result];
+        result.name = "Test";
+        return result;
+    }
+
+    static TParseTree opCall(string input)
+    {
+        return Test(TParseTree(``, false, [], input, 0, 0));
+    }
+    }
+}
+
+alias GenericTest!(ParseTree).Test Test;
+
+/// TODO: modify the way generic rules are written, again.
+/// Caution: memoization code
 void main()
 {
+    writeln(grammar(`
+    Test:
+        A <- "a"
+        B(b) <- b
+        `));
+    
+    writeln(Test("a"));
+    writeln(and!(Test.A, Test.B!(literal!"b"))(ParseTree("",false,[],"ab")));
 }
