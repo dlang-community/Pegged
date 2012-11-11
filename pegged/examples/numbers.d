675eb68dc69f30d89fb39cc0ed336c00fd0cd799
Philippe Sigaud <philippe.sigaud@gmail.com>
2012-11-11 11:47:19 +0100

    New unit tests.

--- pegged/examples/numbers.d
+++ pegged/examples/numbers.d
@@ -17,54 +17,62 @@ unittest
 {
     string[] testNumbers =
     [
-        "0", ".0", "0.", "0.0", ".01",
-        "-0", "+0", "-0.", "+0.", "-0.0", "+0.0", "-.0",  "+.0",
+        "0", "0.0", "0.01",
+        "-0", "+0", "-0.0", "+0.0",
         "0", "1", "2", "3", "4", "5", "6", "7", "8", "9",
         "0123456789",
-        "123", "123.", "123.0", "123.01", "-123", "-123.", "-123.0", "-123.01",
+        "123", "123.0", "123.01", "-123", "-123.0", "-123.01",
         "-123e+12", "-123e-12", "+123e+12", "+123e-12", "-123E+12", "-123E-12", "+123E+12", "+123E-12",
         "123.456e+00", "123.456E+00", "123.456e-00", "123.456E-00",
         "-123.456e+00", "-123.456E+00", "-123.456e-00", "-123.456E-00",
         "+123.456e+00", "+123.456E+00", "+123.456e-00", "+123.456E-00"
     ];
 
-    foreach(number; testNumbers)
+    foreach(i,number; testNumbers)
     {
-        assert(Numbers(number).matches == [number]); // Everything parsed
+        assert(Numbers(number).matches == [number]); // Shall parse
     }
 
     // Failures
-    assert(!Numbers("").successful);
-    assert(!Numbers("abc").successful);
-    assert(!Numbers("+").successful);
-    assert(!Numbers("-").successful);
-    assert(!Numbers("+.").successful);
-    assert(!Numbers("-.").successful);
-    assert(!Numbers(".").successful);
-    assert(!Numbers("--1").successful);
-    assert(!Numbers("++1").successful);
-    assert(!Numbers("+-1").successful);
-    assert(!Numbers("-+1").successful);
-    assert(!Numbers("1e").successful);
-    assert(!Numbers("1e+").successful);
-    assert(!Numbers("1e-").successful);
-    assert(!Numbers("1ee").successful);
+    testNumbers =
+    [
+        ".", ".0", "0.", "123..456",
+        "", "abc", "+", "-", "+.", "-.",
+        "--1", "++1", "+-1", "-+1",
+        "1e", "1e+", "1e-","1ee"
+    ];
+
+    foreach(i,number; testNumbers)
+    {
+        assert(Numbers(number).matches != [number]); // None shall parse
+    }
 
     // Hexadecimal numbers
     testNumbers =
     [
-        "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F",
+        "0", "1", "2", "3", "4", "5", "6", "7", "8", "9",
+        "A", "B", "C", "D", "E", "F",
+        "a", "b", "c", "d", "e", "f",
         "0123456789ABCDEF",
-        "DEADBEEF", "0123BEEF", "DEAD0123"
+        "0123456789abcdef",
+        "DEADBEEF", "0123BEEF", "DEAD0123",
+        "deadbeef", "0123beef", "dead0123",
+        "123E", "123e"
     ];
 
     foreach(number; testNumbers)
     {
-        assert(Numbers(number).matches == [number]); // Everything parsed
+        assert(Numbers.decimateTree(Numbers.Hexa(number)).matches == [number]); // Shall parse
     }
 
     // Hexadecimal failures
-    assert(!Numbers.Hexa("G").successful);
-    assert(!Numbers.Hexa("-1").successful);
+    testNumbers =
+    [
+        "", "G", "g", "-1", "123.456", "123e+100"
+    ];
 
+    foreach(number; testNumbers)
+    {
+        assert(Numbers.decimateTree(Numbers.Hexa(number)).matches != [number]); // None shall parse
+    }
 }
