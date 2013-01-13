4906dacb6f46358307f8c6b99087028960009c48
Philippe Sigaud <philippe.sigaud@gmail.com>
2013-01-13 22:35:02 +0100

    Comparing static and dynamic grammars.
    For this very simple benchmark, comparing
    
    A <~ 'a'* B*
    B <- 'b'
    
    For a static (standard Pegged) and a dynamic (rules are delegates constructed with peggged.dynamic.d) grammar
    the results are astonishing: I get a dynamic parser 10 - 300 times faster than the static one.

--- test.d
+++ test.d
@@ -14,46 +14,72 @@ import std.typetuple;
 import pegged.grammar;
 import pegged.dynamic;
 
-import pegged.examples.dgrammar;
-import dparser;
+//import pegged.examples.dgrammar;
+//import dparser;
 
-/+
-ParseTree foo(ParseTree p)
-{
-    writeln("called!");
-    writeln("[", p.input, "] ", p.begin, " ", p.end);
-    return p;
-}
 
-enum g = (grammar!(Memoization.no)(`Test:
-    A <- 'a'
-    B <- eps {foo} 'b' { foo }
-`));
+enum g = grammar(`Test:
+    A <~ 'a'* B*
+    B <- 'b'
+`);
 
 mixin(g);
-+/
-void main()
+
+struct Test2
 {
-    //asModule!(Memoization.no)("dparser", "dparser", Dgrammar);
-    //writeln(g);
-    Dynamic sp = named(discard(zeroOrMore(or(literal(" "), literal("\t"), literal("\n")))), "sp");
-    //writeln(getName(sp));
-    Dynamic identifier = fuse(oneOrMore(charRange('a', 'z')));
-    Dynamic unless = and( sp, literal("unless")
-                        , sp, literal("("), sp, p => D.IfCondition(p), sp, literal(")")
-                        , sp, p => D.ThenStatement(p));
+    static ParseTree delegate(ParseTree) A;// = named(literal("b"), "Test.B"); //named(and(zeroOrMore(literal("a")), zeroOrMore(B)), "Test.A");
+    static ParseTree delegate(ParseTree) B;// = named(literal("b"), "Test.B");
+
+    static this()
+    {
+        A = fuse(named(and(zeroOrMore(literal("a")), zeroOrMore(B)), "Test.A"));
+        B = named(literal("b"), "Test.B");
+    }
+    static bool isRule(string s)
+    {
+        switch(s)
+        {
+            case "Test.A":
+            case "Test.B":
+                return true;
+            default:
+                return false;
+        }
+    }
+    mixin decimateTree;
+    alias spacing Spacing;
+
+    static ParseTree opCall(ParseTree p)
+    {
+        ParseTree result = decimateTree(A(p));
+        result.children = [result];
+        result.name = "Test";
+        return result;
+    }
+
+    static ParseTree opCall(string input)
+    {
+        return Test(ParseTree(``, false, [], input, 0, 0));
+    }
+
+}
 
 
-    writeln(unless(ParseTree("", true, [], "unless (expr) {}")));
-    //D.beforeIfStatement = unless;
-    ParseTree result = D("
 void main()
 {
-    if (expr) {}
-}");
-    writeln(result);
-    /+Test.afterA = pegged.dynamic.zeroOrMore(toDelegate(&Test.B));
-    auto e = Test("bbba");
-    writeln(e);+/
+    string input = "";
+    int N = 1_000;
+    writeln(Test2(input));
+    foreach(n;0..100)
+    {
+        auto b = benchmark!(()=> Test(input), ()=> Test2(input))(N);
+        auto t1 = b[0].to!("usecs", float)/N;
+        auto t2 = b[1].to!("usecs", float)/N;
+        writefln("%s: %.1f and %.1f => %.1f times faster", input.length, t1, t2, t1/t2);
+        input = "a" ~ input ~ "b";
+    }
+    // Dynamically changing the rules
+    writeln(Test2(input));
+    
 }
 
