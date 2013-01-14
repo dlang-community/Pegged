c03766815bcae9dff742113e501df560f1000a0e
Philippe Sigaud <philippe.sigaud@gmail.com>
2013-01-14 20:27:49 +0100

    Testing dynamic grammars.

--- test.d
+++ test.d
@@ -18,59 +18,143 @@ import pegged.dynamic;
 //import dparser;
 
 
-enum g = grammar(`Test:
-    Root <~ A* B C
+enum g = grammar(`
+
+Test:
+    Root <- (A B C)*
     A <- 'a'
     B <- 'b'
     C <- 'c'
+	Many(R) <- R*
+
 `);
 
 mixin(g);
 
-struct Test2
+struct ParameterizedRule
 {
-    static ParseTree delegate(ParseTree) Root;
-    static ParseTree delegate(ParseTree) A;
-    static ParseTree delegate(ParseTree) B;
-    static ParseTree delegate(ParseTree) C;
+	size_t numArgs;
+	
+	Dynamic opCall(D...)(D rules)
+	{
+	
+	}
+}
 
-    static this()
+struct DynamicGrammar
+{
+	Dynamic[string] rules;
+	string startingRule;
+	Dynamic delegate(Dynamic[] a...)[string] paramRules;
+	string grammarName;
+	
+	ParseTree decimateTree(ParseTree p)
     {
-        Root = named(fuse(pegged.dynamic.and( zeroOrMore(p=>A(p)), (ParseTree p)=>B(p), (ParseTree p)=>C(p))), "Test");
-        A = named(literal("a"), "Test.A");
-        B = named(literal("b"), "Test.B");
-        C = named(literal("c"), "Test.C");
+        if(p.children.length == 0) return p;
+
+        ParseTree[] filterChildren(ParseTree pt)
+        {
+            ParseTree[] result;
+            foreach(child; pt.children)
+            {
+                if (  (child.name.startsWith(grammarName) && child.matches.length != 0)
+                   || !child.successful && child.children.length == 0)
+                {
+                    child.children = filterChildren(child);
+                    result ~= child;
+                }
+                else if (child.name.startsWith("keep!(")) // 'keep' node are never discarded.
+                                               // They have only one child, the node to keep
+                {
+                    result ~= child.children[0];
+                }
+                else // discard this node, but see if its children contain nodes to keep
+                {
+                    result ~= filterChildren(child);
+                }
+            }
+            return result;
+        }
+        p.children = filterChildren(p);
+        return p;
     }
-
-    static ParseTree opCall(ParseTree p)
+	
+    ParseTree opCall(ParseTree p)
     {
-        ParseTree result = Root(p);
+        ParseTree result = decimateTree(rules[startingRule](p));
         result.children = [result];
-        result.name = "Test";
+        result.name = grammarName;
         return result;
     }
 
-    static ParseTree opCall(string input)
+    ParseTree opCall(string input)
     {
-        return Root(ParseTree(``, false, [], input, 0, 0));
+		ParseTree result = decimateTree(rules[startingRule](ParseTree(``, false, [], input, 0, 0)));
+        result.children = [result];
+        result.name = grammarName;
+        return result;
     }
+	
+	void opIndexAssign(D)(D code, string s)
+	{
+		rules[s]= named(code, "Test." ~ s);
+	}
+	
+	Dynamic opIndex(string s)
+	{
+		return rules[s];
+	}
+}
+
+DynamicGrammar Test2()
+{
+	DynamicGrammar dg;
+	
+	dg.grammarName = "Test";
+	// Many(R) <- R*
+    dg.paramRules["Many"] = (Dynamic...) => zeroOrMore(R[0]);
+	// Pair(A,B) <- A B
+	dg.paramRules["Pair"] = (Dynamic[] R...) => and(R[0], R[1]);
+	dg["Root"] = dg.paramRules["Many"](and(()=>dg["A"], ()=>dg["B"], ()=>dg["C"]));
+    dg["A"] = literal("a");
+    dg["B"] = literal("b");
+    dg["C"] = literal("c");
+	
+	dg.startingRule = "Root";
+	
+	return dg;
 }
 
 
+/**
+Dynamic grammars:
+	- no CT parsing
+	- no memoization (could be added)
+	- slightly less handy parameterized rules => ajouter un overload de and and or
+	    qui accepte les Dynamic[],
+	
+Advantages:
+	- fully runtime configurable: change rules, add rules, delete rules.
+*/
 void main()
 {
+	DynamicGrammar test2 = Test2();
+	ParseTree e = (test2("abcabcabc"));
+	writeln(e);
     string input = "";
-    int N = 1000;
+    int N = 200;
+	
+	// Rule1 = Rule2; // Weak linking. If Rule2 changes, Rule1 will point to the old Rule2.
+	// Rule1 = (ParseTree p) => Rule2(p) : Strong linking. If Rule2 evolves, Rule1 will still point to it.
 
-    writeln(Test2(ParseTree("", true, [], input)));
-    foreach(n;0..20)
-    {
-        auto b = benchmark!(()=> Test(input), ()=> Test2(input))(N);
-        auto t1 = b[0].to!("usecs", float)/N;
-        auto t2 = b[1].to!("usecs", float)/N;
-        writefln("%s: %.1f and %.1f => %.1f times faster", input.length, t1, t2, t1/t2);
-        input = "aaaaaaaaaaaa" ~ input ~ "b";
-    }
-    //writeln(Test2(input));
+	foreach(n; 0..10)
+	{
+		auto b = benchmark!(()=> Test(input), ()=>test2(input))(N);
+		auto t1 = b[0].to!("usecs", float)/N;
+		auto t2 = b[1].to!("usecs", float)/N;
+		writefln("%.1f %.1f => %.2f", t1, t2, t2/t1); 
+		input ~= "abcabcabcabcabcabcabcabc";
+	}	
+
 }
 
