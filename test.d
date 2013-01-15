cd7fd26508dd0b32a40deef254402cb4a7efdb10
Philippe Sigaud <philippe.sigaud@gmail.com>
2013-01-15 21:55:03 +0100

    Testing adding an 'unless(cond) { statement }' statement to the Statement rule. It worksgit add test.d

--- test.d
+++ test.d
@@ -1,90 +1,75 @@
 /// Testing Pegged modifications.
 module test;
 
-import std.algorithm;
-import std.array;
-import std.conv;
+//import std.algorithm;
+//import std.array;
+//import std.conv;
 import std.datetime;
-import std.functional;
-import std.range;
+//import std.functional;
+//import std.range;
 import std.stdio;
-import std.typecons;
-import std.typetuple;
+//import std.typecons;
+//import std.typetuple;
 
 import pegged.grammar;
 import pegged.dynamicpeg;
 import pegged.dynamicgrammar;
 
-//import pegged.examples.dgrammar;
-//import dparser;
-
-/+
-enum g = grammar(`
-
-Test:
-    Root <- (A B C)*
-    A <- 'a'
-    B <- 'b'
-    C <- 'c'
-    Many(R) <- R*
-
-`);
-
-mixin(g);
-+/
-DynamicGrammar Test2()
-{
-    DynamicGrammar dg;
-    dg.grammarName = "Test";
-    dg["Root"] = ()=>dg["A"];
-    dg["A"] = literal("a");
-    dg.startingRule = "Root";
-    return dg;
-}
-
-
+import pegged.examples.c;
 /**
 Dynamic grammars:
     - no CT parsing
     - no memoization (could be added)
     - slightly less handy parameterized rules (could be better)
     - no semantic actions (drat, this one is the worse)
+	- no calling from other grammars?
 
 Advantages:
     - fully runtime configurable: change rules, add rules, delete rules.
 */
+
+/**
+TODO: test rule one node.
+*/
 void main()
 {
-    DynamicGrammar dg2 = pegged.dynamicgrammar.grammar("Test:
-    Root <- A B
-    A <- 'a'
-    B <- 'b'
-    C <- 'c'
-    ");
-
-    writeln(dg2("abc"));
-    //writeln(dg2["Root"](ParseTree("", false, [],"abc")));
-    //writeln(dg2["A"](ParseTree("", false, [],"abc")));
-    //writeln(dg2["B"](ParseTree("", false, [],"abc")));
-    //writeln(dg2["C"](ParseTree("", false, [],"abc")));
-    //writeln(getName(dg2["A"]));
+    //writeln(makeSwitch(40));
+    Dynamic[string] predefined =
+    [ "quote":      (ParseTree p) => literal("'")(p)
+    , "doublequote":(ParseTree p) => literal("\"")(p)
+    , "backquote":  (ParseTree p) => literal("`")(p)
+    , "slash":      (ParseTree p) => literal("/")(p)
+    , "backslash":  (ParseTree p) => literal("\\")(p)
+    , "endOfLine":  (ParseTree p) => or(literal("\n"), literal("\r\n"), literal("\r"))(p)
+    , "space":      (ParseTree p) => or(literal(" "), literal("\t"), literal("\n"), literal("\r\n"), literal("\r"))(p)
+    , "digit":      (ParseTree p) => charRange('0', '9')(p)
+    , "identifier": (ParseTree p) => fuse(and( or(charRange('a','z'), charRange('A','Z'), literal("_"))
+                                             , oneOrMore(or(charRange('a','z'), charRange('A','Z'), literal("_"), charRange('0', '9')))))(p)
+    ];
+    DynamicGrammar dg = pegged.dynamicgrammar.grammar(Cgrammar, predefined);
+    auto space = zeroOrMore(or(literal(" "), literal("\t"), literal("\n"), literal("\r\n"), literal("\r")));
+    dg["UnlessStatement"] = and(literal("unless"), space, literal("("), space, dg["Expression"], space, literal(")"), dg["Statement"]);
+    dg["Statement"] = or(dg["UnlessStatement"], dg["Statement"]);
+    writeln(dg(
+"int main()
+{
+    unless (e) { writeln(e);}
+}"));
+    //writeln(makeSwitch(10));
     /+
-    DynamicGrammar test2 = Test2();
-    ParseTree e = (test2("abcabcabc"));
-    writeln(e);
-    string input = "";
-    int N = 1000;
-    // Rule1 = Rule2; // Weak linking. If Rule2 changes, Rule1 will point to the old Rule2.
-    // Rule1 = (ParseTree p) => Rule2(p) : Strong linking. If Rule2 evolves, Rule1 will still point to it.
+    string input = "1";
+	writeln(dg(input));
+    writeln(Arithmetic(input));
 
-    foreach(n; 0..10)
+    int N = 100;
+    foreach(n; 0..50)
     {
-        auto b = benchmark!(()=> Test(input), ()=>test2(input))(N);
+        auto b = benchmark!(()=> Arithmetic(input), ()=>dg(input))(N);
         auto t1 = b[0].to!("usecs", float)/N;
         auto t2 = b[1].to!("usecs", float)/N;
         writefln("%d: %.1f %.1f => %.2f", input.length, t1, t2, t2/t1);
-        input ~= "abcabcabcabcabcabcabcabc";
+        input ~= "+1";
     }
-+/
+    +/
 }
 
