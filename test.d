9477bf4f51f9a2aabc32d249706d1e4bd20d9a1f
Philippe Sigaud <philippe.sigaud@gmail.com>
2013-01-15 06:44:06 +0100

    Testing grammar generation.

--- test.d
+++ test.d
@@ -36,14 +36,8 @@ DynamicGrammar Test2()
 {
     DynamicGrammar dg;
     dg.grammarName = "Test";
-    // Many(R) <- R*
-    dg.paramRules["Many"] = parameterizedRule(1, (Dynamic[] d) => zeroOrMore(d[0]));
-    // Pair(A,B) <- A B
-    dg.paramRules["Pair"] = parameterizedRule(2, (Dynamic[] d) => and(d[0], d[1]));
-    dg["Root"] = dg.paramRules["Many"](and(()=>dg["A"], ()=>dg["B"], ()=>dg["C"]));
+    dg["Root"] = ()=>dg["A"];
     dg["A"] = literal("a");
-    dg["B"] = literal("b");
-    dg["C"] = literal("c");
     dg.startingRule = "Root";
     return dg;
 }
@@ -61,6 +55,20 @@ Advantages:
 */
 void main()
 {
+    DynamicGrammar dg2 = pegged.dynamicgrammar.grammar("Test:
+    Root <- A B
+    A <- 'a'
+    B <- 'b'
+    C <- 'c'
+    ");
+
+    writeln(dg2("abc"));
+    //writeln(dg2["Root"](ParseTree("", false, [],"abc")));
+    //writeln(dg2["A"](ParseTree("", false, [],"abc")));
+    //writeln(dg2["B"](ParseTree("", false, [],"abc")));
+    //writeln(dg2["C"](ParseTree("", false, [],"abc")));
+    //writeln(getName(dg2["A"]));
+    /+
     DynamicGrammar test2 = Test2();
     ParseTree e = (test2("abcabcabc"));
     writeln(e);
@@ -68,7 +76,7 @@ void main()
     int N = 1000;
     // Rule1 = Rule2; // Weak linking. If Rule2 changes, Rule1 will point to the old Rule2.
     // Rule1 = (ParseTree p) => Rule2(p) : Strong linking. If Rule2 evolves, Rule1 will still point to it.
-/+
+
     foreach(n; 0..10)
     {
         auto b = benchmark!(()=> Test(input), ()=>test2(input))(N);
