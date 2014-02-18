943244726e2dacb2243c3317ea9abd3fbc063c22
Philippe Sigaud <philippe.sigaud@gmail.com>
2014-02-18 16:14:08 +0100

    Cleaning test.d up.

--- pegged/dev/test.d
+++ pegged/dev/test.d
@@ -16,32 +16,4 @@ import pegged.grammar;
 
 void main()
 {
-    alias or!(literal!("abc"), charRange!('0','9')) rule;
-    alias defined!(rule, "myRule") myRule;
-
-    assert(getName!(rule)() == `or!(literal!("abc"), charRange!('0','9'))`);
-    assert(getName!(myRule)() == "myRule");
-
-    // Equality on success (except for the name)
-    ParseTree result = rule("abc0");
-    ParseTree myResult = myRule("abc0");
-
-    assert(myResult.successful && result.successful);
-    assert(myResult.name == "myRule");
-    assert(myResult.matches == result.matches);
-    assert(myResult.begin == result.begin);
-    assert(myResult.end == result.end);
-    assert(myResult.children[0] == result);
-
-    // Equality on failure (except for the name)
-    result = rule("_abc");
-    myResult = myRule("_abc");
-
-    assert(!myResult.successful && !result.successful);
-    assert(myResult.name == "myRule");
-    assert(myResult.matches == result.matches);
-    assert(myResult.begin == result.begin);
-    assert(myResult.end == result.end);
-    assert(myResult.children[0] == result);
 }
-
