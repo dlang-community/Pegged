aae7d1b10f7f06386c61d5823eeadec230750e42
Philippe Sigaud <philippe.sigaud@gmail.com>
2014-02-18 22:48:37 +0100

    Removing grammartester import of pegged/examples/arithmetic, to resolve issue #131. Unit tests pass.

--- pegged/tester/grammartester.d
+++ pegged/tester/grammartester.d
@@ -11,36 +11,35 @@ import std.range : retro;
 import std.algorithm : max, splitter;
 import std.string : format, stripRight, removechars, squeeze;
 
-import pegged.examples.arithmetic;
 import pegged.tester.testerparser;
 import pegged.grammar;
 
 class GrammarTester(grammar, string startSymbol)
 {
 	bool soft = false;
-	
+
 	private Appender!string errorTextStream;
 	private int numberTests;
 	private int numberErrors;
 	private string latestDiff;
-	
+
 	@property int testCount()  { return numberTests; }
 	@property int errorCount() { return numberErrors; }
 	@property string errorText() { return errorTextStream.data; }
 	@property string latestDiffText() { return latestDiff; }
-	
+
 	this()
 	{
 		reset();
 	}
-	
+
 	void reset()
 	{
 		errorTextStream = appender!string();
 		numberTests = 0;
 		numberErrors = 0;
 	}
-	
+
 	private void consumeResults(string file, size_t lineNo, bool pass, string diffText )
 	{
 		numberTests++;
@@ -61,56 +60,56 @@ class GrammarTester(grammar, string startSymbol)
 			}
 		}
 	}
-	
+
 	private bool runTest(string file, size_t lineNo,
 		string textToParse, string desiredTreeRepresentation, bool invert )
 	{
 		auto treeGot = grammar.decimateTree(mixin("grammar."~startSymbol~"(textToParse)"));
 		auto treeChecker = TesterGrammar.decimateTree(TesterGrammar.Root(desiredTreeRepresentation));
-		
+
 		/+writefln("%s",treeGot);
 		writefln("");
 		writefln("%s",treeChecker);
 		writefln("");+/
-		
+
 		if ( !treeGot.successful )
-			consumeResults(file, lineNo, false, 
+			consumeResults(file, lineNo, false,
 				grammar.stringof ~ " failed to parse (left-hand-side).  Details:\n" ~ treeGot.toString());
-		
+
 		if ( !treeChecker.successful )
 			consumeResults(file, lineNo, false,
 				"Failed to parse test expectation (right-hand-side). Details:\n" ~ treeChecker.toString());
-		
+
 		if ( treeGot.successful && treeChecker.successful )
 		{
 			auto ctx = getDifferencer(treeGot, treeChecker);
 			latestDiff = ctx.diff();
-			
+
 			bool pass;
 			if ( invert )
 				pass = (ctx.differences != 0);
 			else
 				pass = (ctx.differences == 0);
-			
+
 			consumeResults(file, lineNo, pass, latestDiff);
 			return pass;
 		}
 		else
 			return false;
 	}
-	
+
 	bool assertSimilar(string file = __FILE__, size_t lineNo = __LINE__)
 		( string textToParse, string desiredTreeRepresentation )
 	{
 		return runTest(file, lineNo, textToParse, desiredTreeRepresentation, false);
 	}
-	
+
 	bool assertDifferent(string file = __FILE__, size_t lineNo = __LINE__)
 		( string textToParse, string desiredTreeRepresentation )
 	{
 		return runTest(file, lineNo, textToParse, desiredTreeRepresentation, true);
 	}
-	
+
 	static auto getDifferencer(T,P)( auto ref T treeRoot, auto ref P patternRoot )
 	{
 		Differencer!(T,P) t;
@@ -119,7 +118,7 @@ class GrammarTester(grammar, string startSymbol)
 		return t;
 	}
 }
-	
+
 private struct Differencer(T,P)
 {
 	size_t level = 0;
@@ -130,7 +129,7 @@ private struct Differencer(T,P)
 	Appender!(string[]) leftColumn;
 	Appender!(char[])   centerLine;
 	Appender!(string[]) rightColumn;
-	
+
 	alias T TParseTree;
 	alias P PParseTree;
 
@@ -139,16 +138,16 @@ private struct Differencer(T,P)
 		level = 0;
 		differences = 0;
 		max1stColumnWidth = 0;
-		
+
 		leftColumn  = appender!(string[])();
 		centerLine  = appender!(char[])();
 		rightColumn = appender!(string[])();
-		
+
 		printColumns("  Nodes Found",'|',"  Nodes Expected");
 		printColumns("",             '|',"");
-		
+
 		diffNode(treeRoot,patternRoot);
-		
+
 		string[] lcolumn = leftColumn.data;
 		char[]   center  = centerLine.data;
 		string[] rcolumn = rightColumn.data;
@@ -157,7 +156,7 @@ private struct Differencer(T,P)
 			diffText.put(format("\n%-*s  %c  %s", max1stColumnWidth,
 				lcolumn[i], center[i], rcolumn[i]).stripRight());
 		diffText.put("\n");
-		
+
 		return diffText.data;
 	}
 
@@ -193,7 +192,7 @@ private struct Differencer(T,P)
 					throw new Exception(
 						"It is forbidden to have both unordered and "~
 						"ordered branches from the same node.");
-				
+
 				// Note that we don't dereference the node's children:
 				//   the pattern child will be a Branch statement and
 				//   it will do the dereference for us.
@@ -209,7 +208,7 @@ private struct Differencer(T,P)
 				// TODO: rescanning the visit array this is probably slow.
 				if ( visited[i] )
 					continue;
-				
+
 				traverseUnexpected(&node.children[i]);
 				visited[i] = true;
 			}
@@ -220,7 +219,7 @@ private struct Differencer(T,P)
 		default: assert(0, "Unexpected element in pattern: "~pattern.name);
 		}
 	}
-	
+
 	private void traverseUnexpected( const(T*) node )
 	{
 		assert(node);
@@ -230,15 +229,15 @@ private struct Differencer(T,P)
 			traverseUnexpected(&child);
 		level--;
 	}
-	
+
 	private void diffBranch( const(T*) node, const(P*) pattern, bool[] visited, ref size_t cursor )
 	{
 		assert(pattern);
 		switch ( pattern.name )
 		{
-			
+
 		case "TesterGrammar.OrderedBranch":
-		
+
 			level++;
 			if ( node ) foreach ( patternChild; pattern.children )
 			{
@@ -254,7 +253,7 @@ private struct Differencer(T,P)
 				}
 			}
 			level--;
-			
+
 			break;
 
 		case "TesterGrammar.UnorderedBranch":
@@ -263,7 +262,7 @@ private struct Differencer(T,P)
 			if ( node ) foreach ( i, patternChild; pattern.children )
 			{
 				size_t foundIndex = size_t.max;
-				
+
 				// Cheap hack for now: 0(n^2) scan-in-scan.
 				//
 				// Ideally the parser will generate hints for locating
@@ -271,7 +270,7 @@ private struct Differencer(T,P)
 				//     GrammarName.childLocationHint(
 				//       GrammarName.ruleId!(RuleName),
 				//       GrammarName.ruleId!(ChildRuleName));
-				// Think about it... it should be possible to compile 
+				// Think about it... it should be possible to compile
 				//   this into integer table lookups, thus allowing us
 				//   to do what a compiler does with an AST: vtable
 				//   lookups followed by offset indexing to typed
@@ -291,7 +290,7 @@ private struct Differencer(T,P)
 						break;
 					}
 				}
-			
+
 				if ( foundIndex == size_t.max )
 					diffNode(null, &patternChild);
 				else
@@ -301,13 +300,13 @@ private struct Differencer(T,P)
 				}
 			}
 			level--;
-			
+
 			break;
-		
+
 		default: assert(0, "Unexpected element in pattern: "~pattern.name);
 		}
 	}
-	
+
 	private bool checkIdentifierMatch(const(T*) node, const(P*) pattern)
 	{
 		if ( !node || !pattern )
@@ -315,24 +314,24 @@ private struct Differencer(T,P)
 
 		auto splitNode    = retro(splitter(node.name,'.'));
 		auto splitPattern = retro(splitter(pattern.matches[0],'.'));
-		
+
 		while ( true )
 		{
 			auto nodePathElem = splitNode.front;
 			auto patternPathElem = splitPattern.front;
 			splitNode.popFront();
 			splitPattern.popFront();
-			
+
 			if ( nodePathElem != patternPathElem )
 				return false;
 
 			if ( splitNode.empty || splitPattern.empty )
 				break;
 		}
-		
+
 		return true;
 	}
-	
+
 	private void lineDiff(const(T*) node, const(P*) expected)
 	{
 		if ( !checkIdentifierMatch( node, expected ) )
@@ -343,7 +342,7 @@ private struct Differencer(T,P)
 		else
 			printNodes(node, expected, false);
 	}
-	
+
 	private string getNodeTxt(const(T*) node)
 	{
 		string nodeTxt = "";
@@ -351,7 +350,7 @@ private struct Differencer(T,P)
 			nodeTxt = node.name;
 		return replicate("  ",level) ~ nodeTxt;
 	}
-	
+
 	private string getPatternTxt(const(P*) pattern)
 	{
 
@@ -373,7 +372,7 @@ private struct Differencer(T,P)
 			diffLine = '<';
 		else if ( different )
 			diffLine = '|';
-		
+
 		printColumns( nodeTxt, diffLine, patternTxt );
 	}
 
@@ -395,13 +394,13 @@ unittest
 
 	auto tester = new GrammarTester!(TesterGrammar, "Root");
 	tester.soft = true;
-	
+
 	tester.assertSimilar(`foo`, `Root->Node`);
 	tester.assertDifferent(`foo`, `Root`);
-	
+
 	tester.assertSimilar(`foo->bar`, `Root->Node->OrderedBranch->Node`);
 	tester.assertDifferent(`foo->bar`, `Root->Node`);
-	
+
 	assert(normalizeStr(tester.latestDiff) ==
 normalizeStr(`
   Nodes Found                    |    Nodes Expected
@@ -411,12 +410,12 @@ TesterGrammar.Root               =  Root
     TesterGrammar.OrderedBranch  <
       TesterGrammar.Node         <
 `));
-	
+
 	tester.assertSimilar(`foo->{bar}`, `Root->Node->OrderedBranch->Node`);
 	tester.assertDifferent(`foo->{bar}`, `Root->Node->UnorderedBranch->Node`);
-	
+
 	tester.assertSimilar(`foo~>{bar}`, `Root->Node->UnorderedBranch->Node`);
-	
+
 	tester.assertSimilar(`foo~>bar`, `Root->Node->UnorderedBranch->Node`);
 
 	tester.assertSimilar(`foo->bar->baz`,
@@ -438,7 +437,7 @@ TesterGrammar.Root               =  Root
 			OrderedBranch->Node
 		}
 		`);
-		
+
 	tester.assertSimilar(`foo->{^bar}->baz`,
 		`Root->Node->
 		{
@@ -454,7 +453,7 @@ TesterGrammar.Root               =  Root
 			UnorderedBranch->Node
 		}
 		`);
-		
+
 	tester.assertSimilar(`foo~>{^bar}~>baz`,
 		`Root->Node->
 		{
@@ -462,7 +461,7 @@ TesterGrammar.Root               =  Root
 			UnorderedBranch->Node
 		}
 		`);
-	
+
 	tester.assertSimilar(`
 		FOO~>
 		{
@@ -473,7 +472,7 @@ TesterGrammar.Root               =  Root
 		/* Bar */       ->UnorderedBranch->Node
 		/* baz */           ->OrderedBranch->Node
 		`);
-		
+
 	tester.assertSimilar(`
 		FOO~>
 		{
@@ -497,7 +496,7 @@ TesterGrammar.Root               =  Root
 		/* baz */               ->Node
 		/*     */       }
 		`);
-	
+
 	tester.assertDifferent(`
 		FOO~>
 		{
@@ -518,7 +517,7 @@ TesterGrammar.Root               =  Root
 		/*     */           }
 		/*     */       }
 		`);
-	
+
 	assert(normalizeStr(tester.latestDiff) ==
 normalizeStr(`
   Nodes Found                        |    Nodes Expected
@@ -543,11 +542,11 @@ TesterGrammar.Root                   =  Root
 	assert(tester.testCount > 0);
 	assert(tester.errorCount == 0);
 	assert(tester.errorText == "");
-	
+
 
 	tester = new GrammarTester!(TesterGrammar, "Root");
 	tester.soft = true;
-	
+
 	tester.assertSimilar(`
 		FOO
 		{
@@ -568,13 +567,31 @@ TesterGrammar.Root                   =  Root
 		/*     */           }
 		/*     */       }
 		`);
-	
+
 	assert(tester.testCount == 1);
 	assert(tester.errorCount == 1);
 	assert(tester.errorText.length > 0);
-	
+}
+
+unittest
+{
+    mixin(grammar(`
+    Arithmetic:
+        Term     < Factor (Add / Sub)*
+        Add      < "+" Factor
+        Sub      < "-" Factor
+        Factor   < Primary (Mul / Div)*
+        Mul      < "*" Primary
+        Div      < "/" Primary
+        Primary  < Parens / Neg / Number / Variable
+        Parens   < :"(" Term :")"
+        Neg      < "-" Primary
+        Number   < ~([0-9]+)
+        Variable <- identifier
+    `));
+
 	auto arithmeticTester = new GrammarTester!(Arithmetic, "Term");
-	
+
 	arithmeticTester.assertSimilar(`1 + 3`,
 		`
 		Term->
@@ -584,7 +601,7 @@ TesterGrammar.Root                   =  Root
 				Factor->Primary->Number
 		}
 		`);
-	
+
 	arithmeticTester.assertSimilar(`1*2 + 3/4`,
 		`
 		Term->
@@ -607,20 +624,7 @@ TesterGrammar.Root                   =  Root
 }
 
 /+ For reference:
-mixin(grammar(`
-Arithmetic:
-    Term     < Factor (Add / Sub)*
-    Add      < "+" Factor
-    Sub      < "-" Factor
-    Factor   < Primary (Mul / Div)*
-    Mul      < "*" Primary
-    Div      < "/" Primary
-    Primary  < Parens / Neg / Number / Variable
-    Parens   < :"(" Term :")"
-    Neg      < "-" Primary
-    Number   < ~([0-9]+)
-    Variable <- identifier
-`));
+
 +/
 
 
