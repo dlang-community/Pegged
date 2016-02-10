5a9fc89d1dd1a904cacad09aea4b8c06a8eb3f78
Igor Khasilev <igor.khasilev@revjet.com>
2016-02-10 18:37:13 -0500

    cleanup and filling package control files

--- debian/dlang-pegged.cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the dlang-pegged package
-#
-0 4	* * *	root	[ -x /usr/bin/dlang-pegged_maintenance ] && /usr/bin/dlang-pegged_maintenance
--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(dlang-pegged):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="dlang-pegged" command="/usr/bin/dlang-pegged"
