229ead22412759a1581cad45408b74a69baf69df
Luis Panadero Guardeño <luis.panadero@gmail.com>
2020-04-02 20:50:42 +0200

    Improved appVeyor testing...
    * More info testing on windows (-v flag)
    * dmd >= 2.091.0 launched on 32 and 64 bit on Appveyor
    * Usage of "stable" to pick the last stable releases of dmd and ldc
    * Appveyor show the VM total ram
    * Appveyor uses latest dub version (1.11)
    * Travis and Appveyor by default uses dmd and ldc "stable" versions.
    * Appveyor executes dmd 2.090.1 allowing it to fail, and dmd 2.087.1

--- ci.bat
+++ ci.bat
@@ -5,5 +5,10 @@ setlocal EnableDelayedExpansion
 echo Unit Tests
 REM cd %~dp0\..
 
-echo ""
-dub test --build=unittest-cov
+echo "Test for successful release build"
+dub build -b release --compiler=%DC%
+dub clean --all-packages
+
+echo "Runnin tests"
+dub test --compiler=%DC% -v
+REM --build=unittest-cov
