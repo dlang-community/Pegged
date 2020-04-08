fed222727271aab60a95621418472d843be0f957
Luis Panadero Guardeño <luis.panadero@gmail.com>
2020-04-08 21:52:29 +0200

    Examples are now build and unit tested on CI

--- ci.bat
+++ ci.bat
@@ -3,12 +3,56 @@
 setlocal EnableDelayedExpansion
 
 echo Unit Tests
-REM cd %~dp0\..
 
 echo "Test for successful release build"
 dub build -b release --compiler=%DC%
-dub clean --all-packages
+dub clean --all-packages -q
 
-echo "Runnin tests"
+echo "Running tests"
 dub test --compiler=%DC% -v
-REM --build=unittest-cov
+
+echo "Running example tests"
+dub test :arithmetic --compiler=%DC% 
+dub test :numbers --compiler=%DC% 
+dub test :strings --compiler=%DC%
+dub test :csv --compiler=%DC%
+dub test :json --compiler=%DC%
+
+rem TODO Actually doesn't compiles
+rem pushd examples\c
+rem dub test --compiler=%DC%
+rem popd
+
+pushd examples\dgrammar
+dub test --compiler=%DC%
+popd
+
+pushd examples\markdown
+dub test --compiler=%DC%
+popd
+
+pushd examples\oberon2
+dub test --compiler=%DC%
+popd
+
+pushd examples\parameterized
+dub test --compiler=%DC%
+popd
+
+pushd examples\PEG
+dub test --compiler=%DC%
+popd
+
+pushd examples\peggedgrammar
+dub test --compiler=%DC%
+popd
+
+pushd examples\xml
+dub test --compiler=%DC%
+popd
+
+echo Execute extended_pascal build 
+pushd examples\extended_pascal
+rem Hack to workaround dub bug with preGenerateCommands
+dub build -b release --compiler=%DC% || dub build -b release --compiler=%DC%
+popd
--- ci.sh
+++ ci.sh
@@ -6,20 +6,61 @@ DC=${DC:-dmd}
 
 # test for successful release build
 dub build -b release --compiler=$DC
-dub clean --all-packages
+dub clean --all-packages -q
 
 # test for successful 32-bit build
 if [ "$DC" == "dmd" ]; then
     dub build -b release --compiler=$DC --arch=x86
-    dub clean --all-packages
+    dub clean --all-packages -q
 fi
 
 # Run unit tests
 dub test --compiler=$DC
 
+# Run examples tests
+dub test :arithmetic --compiler=$DC
+dub test :numbers --compiler=$DC
+dub test :strings --compiler=$DC
+dub test :csv --compiler=$DC
+dub test :json --compiler=$DC
+
+# TODO Actually doesn't compiles
+# pushd examples/c
+# dub test --compiler=$DC
+# popd
+
+pushd examples/dgrammar
+dub test --compiler=$DC
+popd
+
+pushd examples/markdown
+dub test --compiler=$DC
+popd
+
+pushd examples/oberon2
+dub test --compiler=$DC
+popd
+
+pushd examples/parameterized
+dub test --compiler=$DC
+popd
+
+pushd examples/PEG
+dub test --compiler=$DC
+popd
+
+pushd examples/peggedgrammar
+dub test --compiler=$DC
+popd
+
+pushd examples/xml
+dub test --compiler=$DC
+popd
+
 # Execute extended_pascal build with time -v
-cd pegged/examples/extended_pascal
+pushd examples/extended_pascal
 # Hack to workaround dub bug with preGenerateCommands
 dub build -b release --compiler=$DC || true
 /usr/bin/time -v dub build -b release --compiler=$DC
+popd
 
