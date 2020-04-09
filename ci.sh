2c4fcb4a0a8714c36fdd29250dfcd91b0dc2585f
Luis Panadero Guardeño <luis.panadero@gmail.com>
2020-04-10 00:43:58 +0200

    Copy old arithmetic example to simple arithmetic

--- ci.sh
+++ ci.sh
@@ -18,47 +18,51 @@ fi
 dub test --compiler=$DC
 
 # Run examples tests
-dub test :arithmetic --compiler=$DC
-dub test :numbers --compiler=$DC
-dub test :strings --compiler=$DC
-dub test :csv --compiler=$DC
-dub test :json --compiler=$DC
+pushd examples/simple_arithmetic
+dub test --compiler=$DC -q
+popd
+
+dub test :arithmetic --compiler=$DC -q
+dub test :numbers --compiler=$DC -q
+dub test :strings --compiler=$DC -q
+dub test :csv --compiler=$DC -q
+dub test :json --compiler=$DC -q
 
 pushd examples/composition
-dub test --compiler=$DC
+dub test --compiler=$DC -q
 popd
 
 # TODO Actually doesn't compiles
 # pushd examples/c
-# dub test --compiler=$DC
+# dub test --compiler=$DC -q
 # popd
 
 pushd examples/dgrammar
-dub test --compiler=$DC
+dub test --compiler=$DC -q
 popd
 
 pushd examples/markdown
-dub test --compiler=$DC
+dub test --compiler=$DC -q
 popd
 
 pushd examples/oberon2
-dub test --compiler=$DC
+dub test --compiler=$DC -q
 popd
 
 pushd examples/parameterized
-dub test --compiler=$DC
+dub test --compiler=$DC -q
 popd
 
 pushd examples/PEG
-dub test --compiler=$DC
+dub test --compiler=$DC -q
 popd
 
 pushd examples/peggedgrammar
-dub test --compiler=$DC
+dub test --compiler=$DC -q
 popd
 
 pushd examples/xml
-dub test --compiler=$DC
+dub test --compiler=$DC -q
 popd
 
 # Execute extended_pascal build with time -v
